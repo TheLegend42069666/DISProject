c6f8cad233311a457704f8b6604c90776a55d3ac
Kevin <k.kove2002@hotmail.dk>
2024-06-04 21:01:11 +0200

    Added the rest of the E/R model to the database

--- serverProject.py
+++ serverProject.py
@@ -13,28 +13,89 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Book(db.Model):
-    bookid = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    releaseDate = db.Column(db.Date, nullable=False)
-    coverImage = db.Column(db.String(255), nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+    cover_image = db.Column(db.String(255), nullable=False)
     summary = db.Column(db.Text, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return f'<Book {self.title}>'
+        return f'<Book: {self.title}>'
 
 class Authors(db.Model):
-    Autorid = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String(100), nullable=False)
+    autor_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f'<Authors {self.Name}>'
+        return f'<Author: {self.name}>'
 
 written_by = db.Table('written_by',
-    db.Column('book_id', db.Integer, db.ForeignKey('book.bookid'), primary_key=True),
-    db.Column('author_id', db.Integer, db.ForeignKey('authors.Autorid'), primary_key=True)
+    db.Column('book_id', db.Integer, db.ForeignKey('book.book_id'), primary_key=True),
+    db.Column('author_id', db.Integer, db.ForeignKey('authors.autor_id'), primary_key=True)
 )
+
+class Genres(db.Model):
+    genre_id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Genre: {self.genre}>'
+
+
+what_genre = db.Table('what_genre',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.book_id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genres.genre_id'), primary_key=True)
+)
+
+
+class Users(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    wallet = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<User: {self.name}>'
+
+
+ledger = db.Table('ledger',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.book_id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genres.genre_id'), primary_key=True),
+    db.Column('loan_id', db.Integer),
+    db.Column('expiry_date', db.Date),
+    db.Column('lend_date', db.Date)
+)
+
+class Admin(db.Model):
+    admin_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user = db.relationship('Users', backref='Admin')
+    events = db.relationship('Events', backref='admin')
+
+    def __repr__(self):
+        return f'<Admin id:{self.admin_id}, User:{self.user.name}>'
+
+
+
+class Events(db.Model):
+    event_id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.admin_id'), nullable=False)
+    type_id = db.Column(db.Integer, db.ForeignKey('event_types.type_id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Event {self.event_id} at {self.timestamp}>'
+
+
+class EventTypes(db.Model):
+    type_id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(100), nullable=False)
+    events = db.relationship('Events', backref='event_type')
+
+    def __repr__(self):
+        return f'<EventType: {self.type_id}, {self.type}>'
+
 
 
 @app.route('/')
